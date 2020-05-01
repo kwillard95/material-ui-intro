2a57e708aedf1c854a6054b09bf7e5efd8cbc562
Kelly Willard <kellyw@kellywiardmbp16.lan>
2020-04-30 19:27:16 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
