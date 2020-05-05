8cf682d916582e3b90bad34979df5bb8eb30ea93
Kelly Willard <kellyw@kellywiardmbp16.lan>
2020-05-05 07:36:07 -0700

    Styles coffee card:

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
@@ -37,7 +38,7 @@ export default function CoffeeCard(props) {
     <Card variant="outlined">
       <CardHeader
         avatar={
-          <Avatar src={avatarSrc} />
+          <Avatar style={{width: '50px'}} src={avatarSrc} />
         }
         action={
           <IconButton aria-label="settings">
@@ -47,24 +48,15 @@ export default function CoffeeCard(props) {
         title={title}
         subheader={subTitle}
       />
+      <CardMedia style={{height: '150px'}} image={imgSrc} title="Best Coffee Maker" />
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
         <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          {description}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Buy Now</Button>
+        <Button size="small">Offer</Button>
       </CardActions>
     </Card>
   );
--- src/Content.jsx
+++ src/Content.jsx
@@ -8,7 +8,10 @@ const Content = () => {
       <Grid item xs={12} sm={4}>
         <CoffeeCard title="Hamilton Beach Flex Brew"
         subTitle="$89.99"
-        avatarSrc="https://target.scene7.com/is/image/Target/GUEST_6e52bb55-a427-4087-a251-b4cc8b4fffd4?wid=488&hei=488&fmt=pjpeg"  />
+        avatarSrc="https://target.scene7.com/is/image/Target/GUEST_6e52bb55-a427-4087-a251-b4cc8b4fffd4?wid=488&hei=488&fmt=pjpeg"
+        imgSrc="https://dynaimage.cdn.cnn.com/cnn/c_fill,g_auto,w_1200,h_675,ar_16:9/https%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F150929101049-black-coffee-stock.jpg"
+        description="Hamilton Beach Brewer is the best way to start your day. Start your morning on the right foot and delight your tastebuds, simultaneously!"
+        />
       </Grid>
       <Grid item xs={12} sm={4}>
         <CoffeeCard />
